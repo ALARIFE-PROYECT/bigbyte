87b46e2b083d9c619fc5d9165a28283b6d6c1755
jesoria <jesoria@indra.es>
2025-07-29 16:46:53 +0200

    feat: loggerBuffer

--- logger/src/service/LoggerBuffer.ts
+++ logger/src/service/LoggerBuffer.ts
@@ -1,3 +1,7 @@
 /**
  * Este servicio accedera a todos los buffers de logs y los concatenara
+ * 
+ * * loggerBuffer from utils => cli thread
+ * * loggerBuffer from utils => main thread
+ * * winstonBuffer from logger => main thread
  */
--- logger/src/service/LoggerService.ts
+++ logger/src/service/LoggerService.ts
@@ -6,30 +6,30 @@ export class LoggerService {
     constructor() { }
 
     public error(...args: Parameters<Logger['error']>) {
-        logger?.error(...args);
+        logger.error(...args);
     }
 
     public warn(...args: Parameters<Logger['warn']>) {
-        logger?.warn(...args);
+        logger.warn(...args);
     }
 
     public info(...args: Parameters<Logger['info']>) {
-        logger?.info(...args);
+        logger.info(...args);
     }
 
     public http(...args: Parameters<Logger['http']>) {
-        logger?.http(...args);
+        logger.http(...args);
     }
 
     public verbose(...args: Parameters<Logger['verbose']>) {
-        logger?.verbose(...args);
+        logger.verbose(...args);
     }
 
     public debug(...args: Parameters<Logger['debug']>) {
-        logger?.debug(...args);
+        logger.debug(...args);
     }
 
     public silly(...args: Parameters<Logger['silly']>) {
-        logger?.silly(...args);
+        logger.silly(...args);
     }
 }
