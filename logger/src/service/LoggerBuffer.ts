b75491c41ca8d3024442c040f4e76fad0734a95c
jesoria <jesoria@indra.es>
2025-07-30 21:04:01 +0200

    feat: logger buffer

--- logger/src/service/LoggerBuffer.ts
+++ logger/src/service/LoggerBuffer.ts
@@ -5,3 +5,25 @@
  * * loggerBuffer from utils => main thread
  * * winstonBuffer from logger => main thread
  */
+
+import { IpcMessage, THREAD_LOG_EMIT, THREAD_LOG_READY } from "@bigbyte/utils/ipc";
+import { logBuffer } from "@bigbyte/utils/logger";
+
+
+
+
+
+const getLogBuffer = (): Promise<Array<string>> => {
+    return new Promise((resolve, reject) => {
+        // Datos de logger del thread del cli
+        process.on("message", (message: IpcMessage) => {
+            if (message.type === THREAD_LOG_EMIT) {
+                return resolve(message.data as Array<string>);
+            }
+        });
+
+        if (process?.send) {
+            process?.send({ type: THREAD_LOG_READY });
+        }
+    });
+}
