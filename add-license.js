350f3dccea7f8da36ee075d4155884820a3f51c2
jesoria <jesoria@indra.es>
2025-09-09 14:28:48 +0200

    automatizaciones

--- add-license.js
+++ add-license.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2025 Jose Eduardo Soria
+ *
+ * This file is part of add-license.
+ *
+ * Licensed under the  (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License in the LICENSE file
+ * at the root of this project.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+ */
+
 /**
  * Script
  *
@@ -10,8 +25,25 @@
 /**
  * Examples:
  *
- * Edit all ts files
+ * Edit /lib ts as js files in the current project: node add-license.js --project=./utils/lib --project-name=@bigbyte/utils --ext=.ts,.js
+ * Edit all ts all projects: node add-license.js --path=./ --ext=.ts
  */
+
+/**
+ * Ejemplos:
+ *
+ * Usado en build:
+ * Modifica el transpilado y agrega la licencia a todos los archivos .ts y .js.
+ * node add-license.js --project=./lib --project-name=@bigbyte/utils --ext=.ts,.js
+ * 
+ * 
+ * Usado en el codigo publico:
+ * Modifica todos los archivos .ts de todos los proyecots del repositorio.
+ * node add-license.js --path=./ --ext=.ts
+ */
+
+const fs = require("fs");
+const path = require("path");
 
 const getArgvValue = (flag) => {
   let result = null;
@@ -26,9 +58,16 @@ const getArgvValue = (flag) => {
   return result;
 };
 
-const rootPath = getArgvValue("path") ?? process.cwd(); // busca todos los proyectos en la ruta actual
-const projectPath = getArgvValue("project") ?? null; // si se especifica un proyecto, solo se procesa ese
+const rootPath = getArgvValue("path") ?? null; // busca todos los proyectos en la ruta actual
+
 const fileExtension = getArgvValue("ext") ?? ".js";
+
+const projectPath = getArgvValue("project") ?? null; // si se especifica un proyecto, solo se procesa ese
+const projectName = getArgvValue("project-name") ?? null; // nombre del proyecto, si no se especifica, se toma del package.json
+
+if(projectPath && !projectName) {
+  throw new Error("You must specify --project-name when using --project");
+}
 
 const getLicenseText = (name) => `/*
  * Copyright (c) 2025 Jose Eduardo Soria
@@ -61,6 +100,10 @@ const writeLicense = (projectName, projectPath, ext) => {
   const licenseText = getLicenseText(projectName);
 
   const processDirectory = (dirPath) => {
+    if (dirPath.includes("node_modules") || dirPath.includes(".git")) {
+      return;
+    }
+
     const items = fs.readdirSync(dirPath);
 
     items.forEach((item) => {
@@ -85,37 +128,48 @@ const writeLicense = (projectName, projectPath, ext) => {
 };
 
 if (rootPath) {
-  const rootPath = path.isAbsolute(rootPath)
+  const rootProjectsPath = path.isAbsolute(rootPath)
     ? rootPath
     : path.join(process.cwd(), rootPath);
-  const rootFiles = fs.readdirSync(rootPath, { withFileTypes: true });
+  const rootFiles = fs.readdirSync(rootProjectsPath, { withFileTypes: true });
 
   const rootDirs = rootFiles.filter((item) => item.isDirectory());
   const projects = [];
   rootDirs.forEach((dir) => {
-    const packagePath = path.join(rootPath, dir.name, "package.json");
+    const packagePath = path.join(rootProjectsPath, dir.name, "package.json");
 
     if (fs.existsSync(packagePath)) {
-      const packageJson = getPackageJson(path.join(rootPath, dir.name));
+      const packageJson = getPackageJson(path.join(rootProjectsPath, dir.name));
 
       projects.push({
         name: dir.name,
-        path: path.join(rootPath, dir.name),
+        path: path.join(rootProjectsPath, dir.name),
         projectName: packageJson.name,
       });
     }
   });
 
   projects.forEach((project) => {
-    writeLicense(project.projectName, project.path, fileExtension);
+    if (fileExtension.includes(",")) {
+      fileExtension.split(",").forEach((ext) => {
+        writeLicense(project.projectName, project.path, ext);
+      });
+    } else {
+      writeLicense(project.projectName, project.path, fileExtension);
+    }
   });
-} else if (projectPath) {
-  const projectPath = path.isAbsolute(projectPath)
+} else if (projectPath && projectName) {
+  const completeProjectPath = path.isAbsolute(projectPath)
     ? projectPath
     : path.join(process.cwd(), projectPath);
-  const packageJson = getPackageJson(projectPath);
 
-  writeLicense(packageJson.name, projectPath, fileExtension);
+  if (fileExtension.includes(",")) {
+    fileExtension.split(",").forEach((ext) => {
+      writeLicense(projectName, completeProjectPath, ext);
+    });
+  } else {
+    writeLicense(projectName, completeProjectPath, fileExtension);
+  }
 } else {
   throw new Error("You must specify either --path or --project");
 }
--- deployment.yalc.js
+++ deployment.yalc.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2025 Jose Eduardo Soria
+ *
+ * This file is part of deployment.yalc.
+ *
+ * Licensed under the Apache 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License in the LICENSE file
+ * at the root of this project.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+ */
+
 import { readdir } from "fs/promises";
 import { join } from "path";
 import { spawn } from "child_process";
