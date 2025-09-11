6b0807cd9d7f071123942b3abb18d13f40cfefad
Jose Eduardo Soria Garcia <pepesoriagarcia99@gmail.com>
2025-09-11 09:08:18 +0200

    feat: automatizacion de licencia

--- cli/src/constant/argv.ts
+++ cli/src/constant/argv.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2025 Jose Eduardo Soria Garcia <alarifeproyect@gmail.com>
+ *
+ * This file is part of @bigbyte/cli.
+ *
+ * Licensed under the Apache-2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License in the LICENSE file
+ * at the root of this project.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+ */
+
 /** * Comando run */
 export const ARGV_COMMAND_RUN = 'run';
 
